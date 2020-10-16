89370779e8147415e293121a7fd0c70cfdb77457
clara <10088152+claraj@users.noreply.github.com>
2020-10-16 12:18:14 -0500

    tests cleanup

--- wishlist/travel_wishlist/tests.py
+++ wishlist/travel_wishlist/tests.py
@@ -3,20 +3,28 @@ from django.urls import reverse
 
 from .models import Place 
 
-class TestHomePageIsEmptyList(TestCase):
+class TestHomePage(TestCase):
 
-    def test_load_home_page_shows_empty_list(self):
-        response = self.client.get(reverse('place_list'))
+    def test_home_page_shows_empty_list_message_for_empty_database(self):
+        home_page_url = reverse('place_list')
+        response = self.client.get(home_page_url)
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
-        self.assertFalse(response.context['places'])
         self.assertContains(response, 'You have no places in your wishlist')
+
+
+class TestVisitedPage(TestCase):
+
+    def test_visited_page_shows_empty_list_message_for_empty_database(self):
+        response = self.client.get(reverse('places_visited'))
+        self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
+        self.assertContains(response, 'You have not visited any places yet')
 
 
 class TestWishList(TestCase):
 
     fixtures = ['test_places']
 
-    def test_view_wishlist_contains_not_visited_places(self):
+    def test_viewing_wishlist_contains_not_visited_places(self):
         response = self.client.get(reverse('place_list'))
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
         
@@ -26,9 +34,12 @@ class TestWishList(TestCase):
         self.assertNotContains(response, 'Moab')
 
 
-    def test_view_places_visited_shows_visited_places(self):
+class TestVisitedList(TestCase):
+
+    fixtures = ['test_places']
+
+    def test_viewing_places_visited_shows_visited_places(self):
         response = self.client.get(reverse('places_visited'))
-        # Check correct template was used
         self.assertTemplateUsed(response, 'travel_wishlist/visited.html')
 
         self.assertNotContains(response, 'Tokyo')
@@ -41,15 +52,17 @@ class TestAddNewPlace(TestCase):
 
     def test_add_new_unvisited_place_to_wishlist(self):
 
-        response = self.client.post(reverse('place_list'), { 'name': 'Tokyo', 'visited': False }, follow=True)
+        add_place_url = reverse('place_list')
+        new_place_data = {'name': 'Tokyo', 'visited': False }
 
+        response = self.client.post(add_place_url, new_place_data, follow=True)
         # Check correct template was used
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
 
         # What data was used to populate the template?
         response_places = response.context['places']
         # Should be 1 item
-        self.assertEqual(len(response_places), 1)
+        self.assertEqual(1, len(response_places))
         tokyo_response = response_places[0]
 
         # Expect this data to be in the database. Use get() to get data with
@@ -58,7 +71,7 @@ class TestAddNewPlace(TestCase):
         tokyo_in_database = Place.objects.get(name='Tokyo', visited=False)
 
         # Is the data used to render the template, the same as the data in the database?
-        self.assertEqual(tokyo_response, tokyo_in_database)
+        self.assertEqual(tokyo_in_database, tokyo_response)
 
         # And add another place - still works?
         response = self.client.post(reverse('place_list'), { 'name': 'Yosemite', 'visited': False}, follow=True)
@@ -69,7 +82,7 @@ class TestAddNewPlace(TestCase):
         # What data was used to populate the template?
         response_places = response.context['places']
         # Should be 2 items
-        self.assertEqual(len(response_places), 2)
+        self.assertEqual(2, len(response_places))
 
         # Expect this data to be in the database. Use get() to get data with
         # the values expected. Will throw an exception if no data, or more than
@@ -94,12 +107,12 @@ class TestAddNewPlace(TestCase):
         response_places = response.context['places']
 
         # Should be 0 items - have not added any un-visited places
-        self.assertEqual(len(response_places), 0)
+        self.assertEqual(0, len(response_places))
 
         # Expect this data to be in the database. Use get() to get data with
         # the values expected. Will throw an exception if no data, or more than
         # one row, matches. Remember throwing an exception will cause this test to fail
-        place_in_database = Place.objects.get(name="Tokyo", visited=True)
+        place_in_database = Place.objects.get(name='Tokyo', visited=True)
 
 
 
@@ -109,25 +122,25 @@ class TestVisitPlace(TestCase):
 
     def test_visit_place(self):
 
-        # visit place pk = 2 ,  New York
-      
-        response = self.client.post(reverse('place_was_visited', args=(2,) ), follow=True)
+        # visit place pk = 2,  New York
+    
+        visit_place_url = reverse('place_was_visited', args=(2, ))
+        response = self.client.post(visit_place_url, follow=True)
 
         # Check correct template was used
         self.assertTemplateUsed(response, 'travel_wishlist/wishlist.html')
 
         # no New York in the response
-        self.assertNotContains(response, "New York")
+        self.assertNotContains(response, 'New York')
 
         # Is New York visited?
         new_york = Place.objects.get(pk=2)
 
         self.assertTrue(new_york.visited)
 
+
     def test_visit_non_existent_place(self):
 
         # visit place pk = 200 
-      
         response = self.client.post(reverse('place_was_visited', args=(200,) ), follow=True)
-
-        self.assertEqual(response.status_code, 404)  # not found
+        self.assertEqual(404, response.status_code)  # not found
